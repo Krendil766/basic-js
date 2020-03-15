d9b86c334563d7209f91ad32707c302103ebc710
Kudzelka <57723217+Krendil766@users.noreply.github.com>
2020-03-15 22:33:36 +0300

    feat:add simple-chain.

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,38 @@
 const chainMaker = {
-  getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  }
+
+    arr: [],
+
+    getLength() {
+        return this.arr.length;
+    },
+
+    addLink(value) {
+        if (value !== undefined) {
+            this.arr.push(`( ${value} )`);
+        } else {
+            this.arr.push(`(  )`);
+        }
+        return this;
+    },
+
+    removeLink(position) {
+        if (typeof position !== 'number' || position >= this.getLength() || position <= 0) {
+            this.arr = [];
+            throw Error();
+        } else {
+            this.arr.splice(position - 1, 1);
+            return this;
+        }
+    },
+    reverseChain() {
+        this.arr.reverse();
+        return this;
+    },
+    finishChain() {
+        let str = this.arr.join('~~');
+        this.arr = [];
+        return str;
+    }
 };
 
 module.exports = chainMaker;
