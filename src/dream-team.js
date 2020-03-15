42ebb56da3e6ba13f587d46984891b7d7499f8cc
Kudzelka <57723217+Krendil766@users.noreply.github.com>
2020-03-15 22:07:52 +0300

    feat:dream-team good code

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,12 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    if (Array.isArray(members)) {
+        let str = '';
+        members.forEach(item => {
+            if (typeof item === 'string') {
+                str = `${str}${item.trim()[0].toUpperCase()}`;
+            };
+        });
+        return str.split('').sort().join('');
+    }
+    return false;
 };
