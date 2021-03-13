499f732cde1352fd6f865e57703626443ee0b673
Krendil766 <krendil766@gmail.com>
2021-03-14 00:26:05 +0300

    feat:add hanoi-towers

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,8 +1,7 @@
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
     let turns = Math.pow(2, disksNumber) - 1;
-    let seconds = turns / (turnsSpeed / 3600);
     return {
         turns: turns,
-        seconds: seconds
-    };
+        seconds: turns / (turnsSpeed / 3600)
+    }
 }
